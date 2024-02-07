f5eaa3ca800ae5e9385f791433d72fac7bd63016
Mazharul Islam <505mazharulislam@gmail.com>
2024-02-07 10:56:01 +0600

    shorthand

--- conversion.js
+++ conversion.js
@@ -5,7 +5,7 @@
  console.log(total);
 
 
-
+//string to intger
 let score = '80';
 let score2 = 56;
 score = parseInt(score);
@@ -13,11 +13,23 @@ score2 = parseInt(score2)
 const totalScore = score + score2;
 console.log(totalScore);
 
-
+//flot
 const SSCGpa = '4.22';
 const HSCGpa = '5.00';
 const sscGpaFlot = parseFloat(SSCGpa)
 const hscGpaflot = parseFloat(HSCGpa)
 const totalGPA = SSCGpa + HSCGpa;
 console.log(totalGPA);
+
+
+//tofixed
+const num11 = 44.333333;
+const result = num11.toFixed(2);
+const fintalResult = parseFloat(result)
+console.log(fintalResult);
+
+//number to string
+const num22 = 22;
+console.log(num22.toString());
+
 
---
+++ shorthand.js
@@ -0,0 +1,11 @@
+let price1 = 10;
+price1 += 20;
+console.log(price1);
+
+let sum = 50; 
+sum += 20;   
+console.log(sum);
+
+let price = 50;
+price ++;
+console.log(price);
